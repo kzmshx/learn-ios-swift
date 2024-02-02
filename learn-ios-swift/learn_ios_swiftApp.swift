cd51433a2e72fa5ba0df5e3e27ff2a7f2dc5b777
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-02-02 21:41:02 +0900

    Initial Commit

---
+++ learn-ios-swift/learn_ios_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_ios_swiftApp.swift
+//  learn-ios-swift
+//
+//  Created by 平田一将 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct learn_ios_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learn-ios-swiftUITests/learn_ios_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  learn_ios_swiftUITestsLaunchTests.swift
+//  learn-ios-swiftUITests
+//
+//  Created by 平田一将 on 2024/02/02.
+//
+
+import XCTest
+
+final class learn_ios_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
