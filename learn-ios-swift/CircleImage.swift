8b20591e0b6ed688e5798fb1df79727788e7646d
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-02-08 18:40:56 +0900

    Add CircleImage

---
+++ learn-ios-swift/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  learn-ios-swift
+//
+//  Created by k_hirata on 2024/02/08.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("yakamon")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
