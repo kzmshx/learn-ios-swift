297f7dea82d781b7f9782af5dcdd44dd2151b08e
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-02-08 18:45:46 +0900

    CircleImage: Resize image

--- learn-ios-swift/CircleImage.swift
+++ learn-ios-swift/CircleImage.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("yakamon")
+            .resizable()
+            .frame(width: 250, height: 250)
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
