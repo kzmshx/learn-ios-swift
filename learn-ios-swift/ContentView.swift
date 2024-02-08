3857896f5d4b9faee2576477a1f498c4e163f049
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-02-08 18:49:17 +0900

    ContentView: Divider, styles to HStack

--- learn-ios-swift/ContentView.swift
+++ learn-ios-swift/ContentView.swift
@@ -22,11 +22,17 @@ struct ContentView: View {
                     .font(.title)
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
             Spacer()
